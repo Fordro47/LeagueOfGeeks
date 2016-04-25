939c674195ef542d98e22b1cc2a84fd66a236340
Jules Schwartz <jschwartz43@gatech.edu>
2016-04-25 10:59:22 -0400

    added pie chart

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var sizeForCircle = function(d) {
 //Gets the value of the drop down menu
 //var statChoice = document.getElementById('selectElementId');
 // load data
-d3.csv("league_data.csv", function(error, data) {
+d3.csv("league_data_item.csv", function(error, data) {
   
   // change string (from CSV) into number format
   data.forEach(function(d) {
@@ -28,6 +28,21 @@ d3.csv("league_data.csv", function(error, data) {
     d.DMG_Taken = +d["Damage Taken"];
     d.Minions = +d["Minions Killed"];
     d.Healing = +d["Total Healing"];
+
+    d.FirstItemName = +d["FirstItemName"];
+    d.SecondItemName = +d["SecondItemName"];
+    d.ThirdItemName = +d["ThirdItemName"];
+    d.FourthItemName = +d["FourthItemName"];
+    d.FifthItemName = +d["FifthItemName"];
+    d.SixthItemName = +d["SixthItemName"];
+
+    d.FirstItemPercent = +d.FirstItemPercent;
+    d.SecondItemPercent = +d.SecondItemPercent;
+    d.ThirdItemPercent = +d.ThirdItemPercent;
+    d.FourthItemPercent = +d.FourthItemPercent;
+    d.FifthItemPercent = +d.FifthItemPercent;
+    d.SixthItemPercent = +d.SixthItemPercent;
+
   });
 
   var scatterplot = d3.select(".scatterplot")
@@ -188,7 +203,44 @@ d3.csv("league_data.csv", function(error, data) {
 
       })
       .on("click", function(d){
-        //TODO
+           var w = 425,                        //width
+            h = 400,                            //height
+            r = 150;                            //radius
+            itemData = [{"label":"FirstItemName", "value":d.FirstItemPercent}, 
+                    {"label":"SecondItemName", "value":d.SecondItemPercent},
+                    {"label":"ThirdItemName", "value":d.ThirdItemPercent},
+                    {"label":"FourthItemName", "value":d.FourthItemPercent},
+                    {"label":"FifthItemName", "value":d.FifthItemPercent},
+                    {"label":"SixthItemName", "value":d.SixthItemPercent}];
+            
+            var vis = d3.select(".piechart")
+                .append("svg:svg")              //create the SVG element inside the <body>
+                .data([itemData])                   //associate our data with the document
+                    .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+                    .attr("height", h)
+                .append("svg:g")                //make a group to hold our pie chart
+                    .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+            var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+                .outerRadius(r);
+            var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+                .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+            var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+                .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+                .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+                    .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+                        .attr("class", "slice");    //allow us to style things in the slices (like text)
+                arcs.append("svg:path")
+                        .attr("fill", function(d, i) { return color(i); } ) //set the color for each slice to be chosen from the color function defined above
+                        .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+                arcs.append("svg:text")                                     //add a label to each slice
+                        .attr("transform", function(d) {                    //set the label's origin to the center of the arc
+                        //we have to make sure to set these before calling arc.centroid
+                        d.innerRadius = 0;
+                        d.outerRadius = r;
+                        return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+                    })
+                    .attr("text-anchor", "middle")                          //center the text on it's origin
+                    .text(function(d, i) { return itemData[i].label; });      //get the label from our original data array
       });
 
   // Dynamic update y-axis variable
@@ -280,7 +332,5 @@ d3.csv("league_data.csv", function(error, data) {
       .attr("fill", "white")
       .style("text-anchor", "end")
       .text(function(d) { return d;});
-
-
 
 });
